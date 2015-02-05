3425f5987b311bec4105f7d6a06a4201cf4cb2c8
brianpchsu <brianpchsu@gmail.com>
2015-02-04 16:16:26 -0800

    Completed funcitonal part of queue and stack.
    In process of functional-shared stack (1 test failed- constructor)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
+// var extend = function(to, from) {
+//   for (var key in from) {
+//     to[key] = from[key];
+//   }
+// };
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.position =0;
+  _.extend(storage, Stack.stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
+
+
+Stack.stackMethods = {};
+
+Stack.stackMethods.push = function(value){
+  console.log(this);
+  this[this.position] = value;
+  this.position++;
+};
+
+Stack.stackMethods.pop = function(){
+  this.position && this.position--;
+  var result = this[this.position];
+  delete this[this.position];
+  return result;
+};
+
+Stack.stackMethods.size = function(){
+  return this.position;
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var remove = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    var result = storage[remove];
+    remove++;
+    return result;
   };
 
   someInstance.size = function(){
+    return size - remove > 0 ? size - remove: 0;
   };
 
   return someInstance;
