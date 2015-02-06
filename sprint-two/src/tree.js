3a380aeb08696770ee58f930bcd44d6b1729c4b6
brianpchsu <brianpchsu@gmail.com>
2015-02-06 12:57:31 -0800

    Finished Tree structure.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -10,29 +10,39 @@ var Tree = function(value){
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(input){
-  var childObject = {value: null};
+  var childObject = new Tree(input);
   this.children[this.children.length] = childObject;
   childObject['value'] = input;
+
 };
 
-treeMethods.contains = function(target){
-  var result = false;
-  for (var i = 0; i<this.children.length; i++){
-    if (this.children[i]['value'] ===target){
-      result = true;
+treeMethods.contains = function(target, current, result){
+  var result ;
+  if (result === undefined){
+    result = false;
+  }
+  else {
+    result = false || result;
+  }
+  var current = current || this;
+
+  if (current['value'] === target){
+    result = true;
+  }
+
+  if(current.children.length>0){
+    for (var i = 0; i<current.children.length; i++){
+      console.log(current.children[i]);
+      result = this.contains(target, current.children[i], result);
+      console.log(result);
     }
   }
+
   return result;
-
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
