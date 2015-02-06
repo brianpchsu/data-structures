f9700020d206e498a3535747d67c15e39d028267
brianpchsu <brianpchsu@gmail.com>
2015-02-05 16:05:56 -0800

    Finished the LinkedList data structure.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,15 +2,43 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var currentEnd = {};
 
   list.addToTail = function(value){
+
+    var newNode = Node(value);
+
+    //Empty condition
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+      currentEnd = newNode;
+    } else {
+      currentEnd.next= newNode;
+      list.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
+
   };
 
   list.contains = function(target){
+    var result = false;
+    var current = list.head;
+    while (current !==null) {
+      if (current.value === target) {
+        result = true;
+      }
+      current = current.next;
+    }
+    return result;
   };
+
 
   return list;
 };
