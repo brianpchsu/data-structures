98ff44aea7405e9b7f2442a35b1c8583b4b1cec1
brianpchsu <brianpchsu@gmail.com>
2015-02-06 20:42:36 -0800

    Almost finished hashTable. Still need to figure out collision.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,35 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  for (var i = 0; i<this._limit; i++){
+      this._storage[i] = [];
+  }
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //this._storage[i] = [];
+  var arrlength = this._storage.storage[i].length;
+  //this._storage[i][arrlength] = [k, v];
+  this._storage.set([i][arrlength], [k, v]);
+
+  // if (this._storage[i][arrlength] === undefined){
+  //   this._storage.set([i][this._storage[i].length], [k, v]);
+  // } else {
+  //   this._storage.set([i][arrlength], [k, v]);
+  //   //console.log("else");
+  // }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i)[1];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(i)[1] = null;
 };
 
 
